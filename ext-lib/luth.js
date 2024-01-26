6b7c720653c8bc84ca164fca32aec17e6664c59a
Lane Sun <lanesun@anlbrain.com>
2024-01-27 02:59:14 +0800

    Ext-Luth: 为 Luth 拓展添加 Ext-Lang 支持

--- ext-lib/luth.js
+++ ext-lib/luth.js
@@ -1,3 +1,35 @@
 export * from "./luth/luth.js";
 import * as luth from "./luth/luth.js";
-globalThis.assign(luth);
+import * as luth_words from "./luth/luth-words.js";
+globalThis.assign(luth_words);
+globalThis.Word = luth.Word;
+globalThis.TraitWord = luth.TraitWord;
+
+String.prototype.assign(TraitWord, {
+    handle(ctx) {
+        const nctx = ctx.extend();
+        for (const c of this)
+            if (c !== nctx.read())
+                return luth.Result.Fail();
+        ctx.merge(nctx);
+        return luth.Result.Success(this);
+    },
+    get_name() {
+        return '"' + this.replaceAll('\n', '↵') + '"';
+    },
+});
+
+RegExp.prototype.assign(TraitWord, {
+    handle(ctx) {
+        const nctx = ctx.extend();
+        const c = nctx.read();
+        if (this.test(c)) {
+            ctx.merge(nctx);
+            return luth.Result.Success(c);
+        } else
+            return luth.Result.Fail();
+    },
+    get_name() {
+        return '/' + this.source.replaceAll('\n', '↵') + '/';
+    },
+});
